44fe043b4f5d08e05b9706a440b4bdc9e522eea6
Juliana kuedjeu <julianakeudjeu@gmail.com>
2025-09-04 13:30:46 +0100

    Add backend folder

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashView from './components/dashboardView';
 
 function App() {
   return (
-    <>
-      <DashView/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard_overview" element={<DashView />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
