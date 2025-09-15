fcfac87c4b8015a076b0ea48393a89144831118b
Fedjio Noumbissi <fedjioguenole@gmail.com>
2025-09-15 12:01:29 +0100

    connecting the feedback

--- React/src/components/about/index.tsx
+++ React/src/components/about/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { User,BriefcaseBusiness,Globe, Award, TrendingUp} from "lucide-react"
+import Navbar from '../../containers/navbar'
 
 const About = () => {
   return (
     <>
     <div>
-      <div className='h-full w-full text-white flex flex-col justify-center align-center items-align insert-none bg-gray-50 '>
+      <Navbar/>
+      <div className='h-full w-full py-20 text-white flex flex-col justify-center align-center items-align insert-none bg-gray-50 '>
       <section className='mb-20 px-12 py-26 flex shadow rounded-bl rounded-br  flex-col justify-center gap-2 align-center items-center bg-primary '>
         <p className='font-bold text-4xl text-center'>About US</p>
         <p className='text-md font-normal text-center'>We connect ambitious professionals with top employers worldwide, creating a bridge between talent and opportunity across industries and borders. Our platform is designed to help job seekers showcase their skills, discover roles that match their career goals.</p>
       </section>
       
       <div className='flex flex-col'>
-      <section className=' m-4 gap-4 px-12 grid grid-rows-4 grid-cols-1 md:grid-rows-2 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-1 gap-4'>
+      <section className=' m-4 gap-4 px-12 grid grid-rows-4 grid-cols-1 md:grid-rows-2 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-1 '>
 
         <div className='transition-transform duration-1000 bg-white hover:scale-110 flex rounded-md p-5 py-12 shadow-md flex-col   cursor-pointer text-primary justify-center items-center  align-center'>
           <TrendingUp className="font-bold w-12 h-12" />
