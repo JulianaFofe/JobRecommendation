f4b72362a454376d093eded8d57345f816ea636b
Njiboy <njisylven51@gmail.com>
2025-09-15 15:27:33 +0100

    touched the help center page

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -24,8 +24,6 @@ import PublicProfile from "./components/profileview";
 import About from "./components/about";
 import Employee_Form from "./components/employee_form/index"
 import AdminFeedback from "./components/feedback/adminsfeedback";
-import Employee_Form from "./components/employee_form/index"
-import AdminFeedback from "./components/feedback/adminsfeedback";
 
 function App() {
   const cards: JobCard[] = [
